55a5c524f9584d52698fe56116a82d1a892a98a5
Artem Polikarpov <artemiypolikarpov@gmail.com>
2021-06-30 12:53:48 +0300

    Fix type error with boards ResCreate

--- libs/common/src/pm-communicator/models/res.model.ts
+++ libs/common/src/pm-communicator/models/res.model.ts
@@ -1,4 +1,5 @@
 import { BoardI } from 'common/pm-communicator/models/entities/board.interface';
+import { BoardDocument } from '../../../../../apps/pm/src/schemas/board.schema';
 
 export type ResGet = BoardI[];
-export type ResCreate = BoardI;
+export type ResCreate = BoardDocument;
